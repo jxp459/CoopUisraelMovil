c1d9df9ba3c995402abcb539136a8aedb1d28817
KerlyNaranjo <k.erly5@hotmail.com>
2020-11-23 23:10:19 -0500

    Agregar vista mapa

---
+++ CoopUisraelSQlite/Mapa.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace CoopUisraelSQlite
+{
+    public partial class Mapa : ContentPage
+    {
+        public Mapa()
+        {
+            InitializeComponent();
+        }
+    }
+}
